bf9c04fbb6e6672ba69538ca83c19acac3f120e9
Luke <lucasgillmore@yahoo.com>
2019-02-06 20:36:14 -0800

    added drawer layout, some simple navigation, and design on weather frag

---
+++ app/src/main/java/team7/tcss450/uw/edu/tcss_750_t7_2/MyMessagesRecyclerViewAdapter.java
@@ -0,0 +1,77 @@
+package team7.tcss450.uw.edu.tcss_750_t7_2;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import team7.tcss450.uw.edu.tcss_750_t7_2.MessageFragment.OnMessageListFragmentInteractionListener;
+import team7.tcss450.uw.edu.tcss_750_t7_2.dummy.DummyContent.DummyItem;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * specified {@link MessageFragment.OnMessageListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyMessagesRecyclerViewAdapter extends RecyclerView.Adapter<MyMessagesRecyclerViewAdapter.ViewHolder> {
+
+    private final List<DummyItem> mValues;
+    private final MessageFragment.OnMessageListFragmentInteractionListener mListener;
+
+    public MyMessagesRecyclerViewAdapter(List<DummyItem> items, OnMessageListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_messages, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onMessageListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public DummyItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.item_number);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
--- app/src/main/java/team7/tcss450/uw/edu/tcss_750_t7_2/WaitFragment.java
+++ app/src/main/java/team7/tcss450/uw/edu/tcss_750_t7_2/WaitFragment.java
@@ -31,7 +31,7 @@ public class WaitFragment extends Fragment {
             mListener = (OnFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement OnWeatherFragmentInteractionListener");
         }
     }
 
